0776cca3ed634c22f88a864b315286cfafaf2a04
Zamur Vedovatto <vedovatto@gmail.com>
2021-03-07 16:37:56 -0300

    choose image from gallery

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function Add() {
-  const [hasPermission, setHasPermission] = useState(null);
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
   const [camera, setCamera] = useState(null);
   const [image, setImage] = useState(null)
   const [type, setType] = useState(Camera.Constants.Type.back);
 
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      const cameraStatus = await Camera.requestPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+
+      const galleryStatus = await ImagePicker.requestCameraPermissionsAsync();
+      setHasGalleryPermission(galleryStatus.status === 'granted');
+      // if (galleryStatus.status !== 'granted') {
+      //   alert('Sorry, we need camera roll permissions to make this work!')
+      // }
     })();
   }, []);
 
@@ -22,10 +30,23 @@ export default function Add() {
     }
   }
 
-  if (hasPermission === null) {
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+    console.log(result)
+    if(!result.cancelled) {
+      setImage(result.uri)
+    }
+  }
+
+  if (hasCameraPermission === null || hasGalleryPermission === null) {
     return <View />;
   }
-  if (hasPermission === false) {
+  if (hasCameraPermission === false || hasGalleryPermission === false) {
     return <Text>No access to camera</Text>;
   }
   return (
@@ -52,6 +73,10 @@ export default function Add() {
       <Button
         title="Take Picture"
         onPress={() => takePicture()}
+      />
+      <Button
+        title="Pick Image From Gallery"
+        onPress={() => pickImage()}
       />
       {image &&  <Image source={{ uri: image }} style={styles.image} />}
     </View>
